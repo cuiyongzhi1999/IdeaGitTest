6be3baf221bec18493f4177cb2fa31790d829ea3
cuiyongzhi1999 <954387362@qq.com>
2021-10-23 22:00:11 +0800

    准备提交

--- src/main/java/com/cyz/springbootdemo/controller/HelloSpringBoot.java
+++ src/main/java/com/cyz/springbootdemo/controller/HelloSpringBoot.java
@@ -19,6 +19,7 @@ public class HelloSpringBoot {
         System.out.println("更新1次");
         System.out.println("创建分支");
         System.out.println("修改dev分支");
+        System.out.println("准备提交");
         return "Hello Spring Boot";
     }
 }
